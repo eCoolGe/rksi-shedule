97843a78349f2b47c865a760474aada22d1a538d
eCoolGe <eCoolGe@mail.ru>
2023-05-04 02:46:38 +0300

    add `cacheControl` from `express-cache-controller`

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Application } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import cacheControl from 'express-cache-controller';
 
 const app: Application = express();
+
+// Задаем настройки cache-control middleware для всех маршрутов
+app.use(cacheControl({
+    maxAge: 3600 // Время в секундах, на которое кэшируется ответ сервера
+}));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
